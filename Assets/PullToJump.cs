8658cd8ca087742938c78acdb94312e8acaf671c
harshdulani <harshdulani@yahoo.in>
2020-03-04 23:29:56 +0530

    initial commit - added pull to jump game mechanic

---
+++ Assets/PullToJump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullToJump : MonoBehaviour
+{
+    public float forceMultiplier = 10f;
+
+    private Rigidbody rb;
+    private Vector3 startPos, endPos;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            startPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            //print("start position = " + startPos);
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            endPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            //print("end position = " + endPos);
+            //print("distance is = " + (startPos - endPos));
+
+            var forceVector = new Vector3((startPos - endPos).x, (startPos - endPos).y, (startPos - endPos).y) * forceMultiplier;
+
+            rb.AddForce(forceVector);
+            print("force vector = " + forceVector);
+        }
+    }
+}
